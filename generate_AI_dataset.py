973696c39e4fc1764b9b43c273ddeee863c21ba8
Amogh <axm0754@mavs.uta.edu>
2025-04-24 17:25:12 -0500

    add script to generate AI dataset images from labels

---
+++ generate_AI_dataset.py
@@ -0,0 +1,25 @@
+import json
+import time
+
+from openai import OpenAI
+
+from utilities import get_key, create_folder_for_label, convert_and_save_images, generate_image
+
+with open("Labels.json", "r") as file:
+    dataset_labels = json.load(file)
+
+labels = dataset_labels.values()
+
+openai_client = OpenAI(api_key=get_key())
+
+for j, label in enumerate(list(labels)):
+    print(f"Working on label {j + 1}/{len(labels)}: {label}")
+    folder_path = create_folder_for_label(label, base_path="dataset/ai")
+
+    twenty_images = generate_image(openai_client,
+                                   prompt=f"Generate a realistic photo of a {label} in its natural habitat.", n=5,
+                                   size="256x256")
+
+    convert_and_save_images(twenty_images, folder_path, label)
+
+    time.sleep(60)
